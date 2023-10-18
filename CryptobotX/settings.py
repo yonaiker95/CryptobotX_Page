97c784c1618f91363aadc3c900fb6a7d5b078c30
CodigoDev <comercio-principal@hotmail.com>
2023-10-17 22:21:04 -0400

    Release Page CryptobotX 1.0

--- CryptobotX/settings.py
+++ CryptobotX/settings.py
@@ -24,14 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = []
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-    print(ALLOWED_HOSTS)
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -133,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if not DEBUG:
+if DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
