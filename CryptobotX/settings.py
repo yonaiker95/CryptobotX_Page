1534f7f2ed7f96afef331f2b1faf0a7112891fa5
CodigoDev <comercio-principal@hotmail.com>
2023-10-17 23:13:05 -0400

    Release Page CryptobotX 1.0

--- CryptobotX/settings.py
+++ CryptobotX/settings.py
@@ -127,12 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'home/static')
 # if not DEBUG:
     # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MEDIA_URL = "/public/"
-MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
