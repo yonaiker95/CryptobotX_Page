803bd4523981583a8a37a27722813b9e00bcf7d7
CodigoDev <comercio-principal@hotmail.com>
2023-10-17 21:54:01 -0400

    Release Page CryptobotX 1.0

--- CryptobotX/settings.py
+++ CryptobotX/settings.py
@@ -24,15 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'RENDER' not in os.environ
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
+    print(ALLOWED_HOSTS)
 
 # Application definition
 
@@ -87,9 +86,6 @@ DATABASES = {
         'NAME': 'CryptobotX',
         "CLIENT": {
             "host": "mongodb+srv://kaoru:Kaoru35.82@cluster0.z1sfv.mongodb.net/",
-            # 'username':'kaoru',
-            # 'password':'kaoru35.82',
-
         }
     }
 }
